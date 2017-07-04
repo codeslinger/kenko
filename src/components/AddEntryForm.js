9839ac22e0b7a3ee34dd02a1586d24fe6b8019b9
Toby DiPasquale <toby@cbcg.net>
2017-07-04 13:36:09 -0400

    Add entry form looking good

---
+++ src/components/AddEntryForm.js
@@ -0,0 +1,118 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import {
+  Form,
+  Grid,
+  Icon,
+  Segment,
+} from 'semantic-ui-react';
+
+export default class AddEntryForm extends Component {
+  static propTypes = {
+    recipes: PropTypes.object.isRequired,
+    visible: PropTypes.bool,
+    onClose: PropTypes.func,
+  };
+  static defaultProps = {
+    visible: true,
+    onClose: null,
+  };
+
+  state = {
+    what: -1,
+    amount: 1,
+    units: null,
+    unitsVisible: false,
+  };
+
+  clearForm = () => this.setState({what: -1, amount: 0, units: '', unitsVisible: false});
+
+  handleChange = (e, {name, value}) => this.setState({[name]: value});
+
+  handleWhatChange = (e, {name, value}) => this.setState({[name]: value, unitsVisible: true});
+
+  handleSubmit = () => this.clearForm();
+
+  onClose = () => {
+    if (this.props.onClose()) {
+      this.props.onClose();
+    }
+    this.clearForm();
+  };
+
+  recipeOptions = (recipes) => Object.keys(recipes).map(
+    (key, i) => {
+      return {
+        key: i,
+        text: recipes[key].label,
+        value: key
+      };
+    }
+  );
+
+  recipeUnits = (recipes, key, units) => {
+    if (key === -1) {
+      return {unitOptions: [], defaultUnit: null};
+    }
+    let unitValue = units;
+    if (!unitValue) {
+      unitValue = 'oz';
+    }
+    return {
+      unitOptions: [
+        {key: 1, text: "g", value: "g"},
+        {key: 2, text: "cup", value: "cup"},
+        {key: 3, text: "oz", value: "oz"},
+      ],
+      unitValue: unitValue,
+    };
+  };
+
+  render() {
+    const {what, amount, units, unitsVisible} = this.state;
+    const {recipes, visible} = this.props;
+    const classNames = classnames({"not-visible": !visible});
+    const recipeOptions = this.recipeOptions(recipes);
+    const {unitOptions, unitValue} = this.recipeUnits(recipes, what, units);
+
+    return (
+      <Segment className={classNames}>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column textAlign="right" verticalAlign="top">
+              <Icon link name="close" size="large" onClick={this.onClose} />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group widths='equal'>
+            <Form.Select label="Search"
+                         name="what"
+                         value={what}
+                         search
+                         scrolling
+                         noResultsMessage='No results found. Try another search.'
+                         placeholder='Search foods'
+                         options={recipeOptions}
+                         onChange={this.handleWhatChange} />
+          </Form.Group>
+          {unitsVisible &&
+            <Form.Group widths='equal'>
+              <Form.Input label="How much?"
+                          name="amount"
+                          value={amount}
+                          onChange={this.handleChange} />
+              <Form.Select label='&nbsp;'
+                           name="units"
+                           value={unitValue}
+                           options={unitOptions}
+                           onChange={this.handleChange} />
+            </Form.Group>
+          }
+          <Form.Button>Add</Form.Button>
+        </Form>
+      </Segment>
+    );
+  }
+}
--- src/components/JournalActions.js
+++ src/components/JournalActions.js
@@ -1,25 +1,16 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import {
-  Button,
-  Container,
-  Form,
-  Grid,
-  Icon,
-  Input,
-  Segment,
-  Select,
-} from 'semantic-ui-react';
+import {Button, Container} from 'semantic-ui-react';
+import AddEntryForm from './AddEntryForm';
 
-class JournalActions extends Component {
+export default class JournalActions extends Component {
   static propTypes = {
     recipes: PropTypes.object.isRequired,
   };
 
   state = {
     addEntryActive: false,
-    addRecipeActive: false,
   };
 
   pauseEvent = (event) => {
@@ -33,12 +24,6 @@ class JournalActions extends Component {
     }
   };
 
-  closeAddRecipe = () => {
-    if (this.state.addRecipeActive) {
-      this.setState({addRecipeActive: false});
-    }
-  };
-
   openAddEntry = (event) => {
     if (!this.state.addEntryActive) {
       this.setState({addEntryActive: true});
@@ -46,74 +31,23 @@ class JournalActions extends Component {
     this.pauseEvent(event);
   };
 
-  openAddRecipe = (event) => {
-    if (!this.state.addRecipeActive) {
-      this.setState({addRecipeActive: true});
-    }
-    this.pauseEvent(event);
-  };
-
   renderActions() {
     let actionClassNames = classnames("journal-actions", {
-      "not-visible": this.state.addEntryActive || this.state.addRecipeActive,
+      "not-visible": this.state.addEntryActive,
     });
 
     return (
       <Container className={actionClassNames}>
         <Button onClick={this.openAddEntry}>New Entry</Button>
-        <Button onClick={this.openAddRecipe}>New Recipe</Button>
       </Container>
     );
   }
 
   renderAddEntryForm(recipes) {
-    const addEntryClassNames = classnames({
-      "not-visible": !this.state.addEntryActive,
-    });
-    const recipeOptions = Object.keys(recipes).map(
-      (key, i) =>
-        { return {key: i, text: recipes[key].label, value: key}; }
-    );
-    const unitOptions = [
-      {key: 1, text: "g", value: "g"},
-      {key: 2, text: "cup", value: "cup"},
-      {key: 3, text: "oz", value: "oz"}
-    ];
-    const now = new Date();
+    const visible = this.state.addEntryActive;
 
     return (
-      <Segment className={addEntryClassNames}>
-        <Grid>
-          <Grid.Row>
-            <Grid.Column textAlign="right" verticalAlign="top">
-              <Icon link name="close" size="large" onClick={this.closeAddEntry} />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-        <Form>
-          <Form.Field control={Select} label="What?" options={recipeOptions} placeholder="Choose a recipe" />
-          <Form.Field control={Input} label="When?" placeholder={now.toDateString()} />
-          <Form.Group widths="equal">
-            <Form.Field control={Input} label="How much?" />
-            <Form.Field control={Select} options={unitOptions} label="Units" />
-          </Form.Group>
-          <Form.Field control={Button}>Add Entry</Form.Field>
-        </Form>
-      </Segment>
-    );
-  }
-
-  renderAddRecipeForm() {
-    let addRecipeClassNames = classnames("journal-add-recipe center", {
-      "not-visible": !this.state.addRecipeActive,
-    });
-
-    return (
-      <div className={addRecipeClassNames}>
-        <h5>Add Recipe</h5>
-        <br/>
-        <a href="#" onClick={this.closeAddRecipe}><i className="material-icons">close</i></a>
-      </div>
+      <AddEntryForm recipes={recipes} visible={visible} onClose={this.closeAddEntry} />
     );
   }
 
@@ -124,10 +58,7 @@ class JournalActions extends Component {
       <div className="row">
         {this.renderActions()}
         {this.renderAddEntryForm(recipes)}
-        {this.renderAddRecipeForm()}
       </div>
     );
   }
 }
-
-export default JournalActions;
